a5194f2b392859218117e8a92696e79b234bd3de
Geoffroy Empain <geoffroy@charlie-bravo.be>
2020-12-05 02:25:50 +0100

    feat: make email config optional

--- src/emails/emails.ts
+++ src/emails/emails.ts
@@ -1,12 +1,33 @@
-import nodemailer, { Transporter, TransportOptions } from 'nodemailer';
+import nodemailer, { SentMessageInfo, TransportOptions } from 'nodemailer';
 import { env } from '../env';
+import Mail from 'nodemailer/lib/mailer';
+import { Logger } from '../commons/logger/logger';
+import chalk from 'chalk';
 
-// https://nodemailer.com/smtp/#general-options
-export const emails: Transporter = nodemailer.createTransport(<TransportOptions>{
-  host: env.MELI_MAIL_HOST,
-  port: env.MELI_MAIL_PORT,
-  auth: env.MELI_MAIL_USERNAME && env.MELI_MAIL_PASSWORD ? {
-    user: env.MELI_MAIL_USERNAME,
-    pass: env.MELI_MAIL_PASSWORD,
-  } : undefined,
-});
+const logger = new Logger('meli.server:emails');
+
+let transporter: {
+  sendMail(mailOptions: Mail.Options): Promise<SentMessageInfo>;
+};
+
+if (env.MELI_MAIL_HOST && env.MELI_MAIL_PORT) {
+  logger.debug('Emails are configured');
+  // https://nodemailer.com/smtp/#general-options
+  transporter = nodemailer.createTransport(<TransportOptions>{
+    host: env.MELI_MAIL_HOST,
+    port: env.MELI_MAIL_PORT,
+    auth: env.MELI_MAIL_USERNAME && env.MELI_MAIL_PASSWORD ? {
+      user: env.MELI_MAIL_USERNAME,
+      pass: env.MELI_MAIL_PASSWORD,
+    } : undefined,
+  });
+} else {
+  logger.warn(`Emails are ${chalk.red('disabled')}, will print to console`);
+  transporter = {
+    async sendMail(mailOptions: Mail.Options): Promise<SentMessageInfo> {
+      logger.info('email', JSON.stringify(mailOptions, null, 2));
+    },
+  };
+}
+
+export const emails = transporter;
