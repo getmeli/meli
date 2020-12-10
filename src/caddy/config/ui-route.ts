eb09f411e9713fe3a53fcfff5ee18d740add0c95
Geoffroy Empain <geoffroy@charlie-bravo.be>
2020-12-10 17:23:57 +0100

    fix: ui responds only to /

--- src/caddy/config/ui-route.ts
+++ src/caddy/config/ui-route.ts
@@ -4,18 +4,40 @@ import { URL } from 'url';
 
 const meliuihost = new URL(env.MELI_UI_URL);
 
-export const uiRoute = {
+export const uiRoute = env.MELI_UI_DIR ? {
   group: 'ui',
-  match: [{
-    host: [meliuihost.hostname],
-    path: ['/*'],
-  }],
+  match: [
+    {
+      host: [meliuihost.hostname],
+      file: {
+        root: env.MELI_UI_DIR,
+        try_files: [
+          '{http.request.uri.path}',
+          '/index.html',
+        ],
+      },
+    },
+  ],
   handle: [
-    // https://caddyserver.com/docs/json/apps/http/servers/routes/handle/reverse_proxy/
-    env.MELI_UI_DIR ? {
+    {
+      handler: 'rewrite',
+      uri: '{http.matchers.file.relative}',
+    },
+    {
       handler: 'file_server',
       root: env.MELI_UI_DIR,
-    } : {
+    },
+  ],
+} : {
+  group: 'ui',
+  match: [
+    {
+      host: [meliuihost.hostname],
+      path: ['/*'],
+    },
+  ],
+  handle: [
+    {
       handler: 'reverse_proxy',
       upstreams: [{
         dial: getReverseProxyDial(env.MELI_UI_URL_INTERNAL.toString()),
