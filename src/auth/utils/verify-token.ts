01dbcc7dcfb71441b735b9c1130c621c87fe44b3
Geoffroy Empain <geoffroy@charlie-bravo.be>
2020-12-10 18:27:22 +0100

    fix(auth): catch verify token error

--- src/auth/utils/verify-token.ts
+++ src/auth/utils/verify-token.ts
@@ -1,14 +1,24 @@
 import { User, Users } from '../../entities/users/user';
 import { env } from '../../env/env';
-import { JwtToken } from '../auth';
 import jwt from 'jsonwebtoken';
 import { promisify } from 'util';
 import { UnauthorizedError } from '../../commons/errors/unauthorized-error';
+import { Logger } from '../../commons/logger/logger';
 
 const verify = promisify(jwt.verify);
 
+const logger = new Logger('meli.api:verifyToken');
+
 export async function verifyToken(token: string): Promise<User> {
-  const { userId, issuedAt }: JwtToken = await verify(token, env.MELI_JWT_SECRET, {});
+  let userId: string;
+  let
+    issuedAt: number;
+  try {
+    ({ userId, issuedAt } = await verify(token, env.MELI_JWT_SECRET, {}));
+  } catch (e) {
+    logger.debug('Error trying to verify token', e);
+    throw new UnauthorizedError('Invalid token');
+  }
 
   const user = await Users().findOne({
     _id: userId,
