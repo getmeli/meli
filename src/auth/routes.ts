e4e925524216b44092020c607de4e8e51b20e1f4
Nikhil Jha <hi@nikhiljha.com>
2020-12-13 14:08:24 -0800

    add routes to oidc endpoints

--- src/auth/routes.ts
+++ src/auth/routes.ts
@@ -5,6 +5,7 @@ import { signOut } from './handlers/sign-out';
 import { apiEndpoint } from '../entities/api/api-endpoint';
 import { getAuthMethods } from './handlers/get-auth-methods';
 import { google_callback, google_redirect } from './passport/google';
+import { oidc_callback, oidc_redirect } from './passport/oidc';
 import { gitea_callback, gitea_redirect } from './passport/gitea';
 import { github_callback, github_redirect } from './passport/github';
 import { gitlab_callback, gitlab_redirect } from './passport/gitlab';
@@ -18,7 +19,7 @@ const passportOptions = {
 };
 
 // TODO could this be moved to the "new GoogleStrategy(...)" ?
-const googleOptions = {
+const oidOptions = {
   scope: [
     'userinfo.profile',
     'userinfo.email',
@@ -30,8 +31,10 @@ router.get(gitlab_redirect, passport.authenticate('gitlab'));
 router.get(gitlab_callback, passport.authenticate('gitlab', passportOptions), authenticate, redirectToUi);
 router.get(gitea_redirect, passport.authenticate('gitea'));
 router.get(gitea_callback, passport.authenticate('gitea', passportOptions), authenticate, redirectToUi);
-router.get(google_redirect, passport.authenticate('google', googleOptions));
+router.get(google_redirect, passport.authenticate('google', oidOptions)); // google is a special case of oidc
 router.get(google_callback, passport.authenticate('google', passportOptions), authenticate, redirectToUi);
+router.get(oidc_redirect, passport.authenticate('oidc', oidOptions));
+router.get(oidc_callback, passport.authenticate('oidc', passportOptions), authenticate, redirectToUi);
 router.get(github_redirect, passport.authenticate('github'));
 router.get(github_callback, passport.authenticate('github', passportOptions), authenticate, redirectToUi);
 router.post('/auth/in-memory', passport.authenticate('in-memory', passportOptions), authenticate, noContent);
