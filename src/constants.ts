f263f090b9c40bc481ed1979a1ae87b51975d035
Geoffroy Empain <geoffroy@charlie-bravo.be>
2021-03-09 12:53:42 +0100

    feat: forbid underscores in site name (#193)

--- src/constants.ts
+++ src/constants.ts
@@ -12,5 +12,5 @@ export const STRING_MAX_LENGTH = 1000;
 export const LONG_STRING_MAX_LENGTH = 5000;
 export const ARRAY_MAX = 1000;
 export const STRIPE_SIGNATURE_HEADER = 'stripe-signature';
-export const SUBDOMAIN_PATTERN = /^[a-z0-9_]?[a-z0-9_-]*[a-z0-9]{1}$/;
+export const SUBDOMAIN_PATTERN = /^[a-z0-9]?[a-z0-9-]*[a-z0-9]{1}$/;
 export const COLOR_PATTERN = /^#[a-z0-9]{6}$/;
