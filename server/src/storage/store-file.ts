3181cab9cd1a9cb7208405fa74ab1f341cb22c8e
Geoffroy Empain <geoffroy@charlie-bravo.be>
2021-05-25 21:20:13 +0200

    fix: cannot upload logo #230

--- server/src/storage/store-file.ts
+++ server/src/storage/store-file.ts
@@ -15,7 +15,7 @@ export async function storeFile(file: Express.Multer.File): Promise<StoredFile> 
   const fileId = uuid();
   const filePath = getFilePath(fileId);
   await promises.mkdir(env.MELI_STORAGE_DIR, { recursive: true });
-  await promises.rename(path.join(file.destination, file.filename), filePath);
+  await promises.copyFile(path.join(file.destination, file.filename), filePath);
   return {
     id: fileId,
     type: file.mimetype,
